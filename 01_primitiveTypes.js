6ae298576066fb4749b2f890fc1ab4c680938580
dskhankal0121 <132569498+dskhankal0121@users.noreply.github.com>
2023-06-07 09:52:38 +0530

    Create 01_primitiveTypes.js

---
+++ 01_primitiveTypes.js
@@ -0,0 +1,13 @@
+
+
+var age=23;
+var weight=45;
+console.log(age);
+console.log(weight);
+var typeOfAge= typeof age;
+console.log(typeOfAge);
+console.log(typeof weight);
+
+var isMarride=true;
+console.log(isMarride);
+console.log(typeof isMarride);
