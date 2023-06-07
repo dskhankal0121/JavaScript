a4fc6845ed7179796c4d5772818b0d7948719f16
dskhankal0121 <132569498+dskhankal0121@users.noreply.github.com>
2023-06-07 09:56:36 +0530

    Create hello.js

---
+++ hello.js
@@ -0,0 +1,13 @@
+console.log("Shubhashree");
+console.log("Dhanashree Kale");
+console.log("A/p Pune");
+var firstname="Dhanashree";
+console.log(firstname);
+
+var firstname="Dhanashree Shubham";
+console.log(firstname);
+var holder;
+console.log(holder);
+firstname="kale";
+console.log(firstname);
+var pinCode;
